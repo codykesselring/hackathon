784a603ca4e20674964fc2ab5c11a5d5a0cbbabd
Jsnit <jrsnitbro@gmail.com>
2023-11-04 13:40:44 -0700

    Fixed formatting

--- Snake.java
+++ Snake.java
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-package hackathon;
-
-
-import java.util.ArrayList;
-import jdk.internal.net.http.common.Pair;
-
-public class Snake {
-    ArrayList<Pair<Integer, Integer>> occupiedSpaces; // coordinates occupied by the snake on the grid
-    Pair<Integer, Integer> headIndex; // coordinates of the head of the snake on the grid
-    char direction; // 'w' = up, 'a' = left, 's' = down, 'd' = right
-    int length; // length of the snake
-
-    public Snake() { // DVC initializing snake to 1 unit long, at (0,0) the top left corner of the grid, facing down
-        occupiedSpaces = new ArrayList<Pair<Integer, Integer>>();
-        headIndex = new Pair<Integer, Integer>(0, 0);
-        occupiedSpaces.add(headIndex);
-        direction = 'd';
-        length = occupiedSpaces.size();
-    }
-    public Snake(Pair<Integer, Integer> headInput) {
-        occupiedSpaces = new ArrayList<Pair<Integer, Integer>>();
-        headIndex = new Pair<Integer, Integer>(headInput.first, headInput.second);
-
-    }
-
-}
-=======
 package hackathon;
 
 
@@ -102,4 +74,3 @@ public class Snake {
             deleteFromEnd();
     }
 }
->>>>>>> 3fa8b92be61897a60ec6b9d58594d1d0007d33fb
