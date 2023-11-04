eebe30459101bad448146a28008f3fd62836891d
Cody Kesselring <codykesselring@gmail.com>
2023-11-04 13:29:54 -0700

    balls

--- Board.java
+++ Board.java
@@ -8,7 +8,7 @@ import java.awt.event.*;
 
 public class Board {
 
-    public void initBoard(int x){
+    public void initBoard(int x, int posX, int posY, int[][] gameArray){
         JFrame frame = new JFrame("Snake Game");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       
@@ -19,12 +19,20 @@ public class Board {
         for (int i = 0; i < x; i++) {
             for (int j = 0; j < x; j++) {
                 JPanel cell = new JPanel();
-                cell.setPreferredSize(new Dimension(x, x));
-                cell.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
+                cell.setPreferredSize(new Dimension(20, 20));
+                cell.setBorder(BorderFactory.createLineBorder(Color.black));
                 panel.add(cell);
-                cell.setBackground(Color.green);
+                gameArray[i][j] = 0; // 0 = empty cell
+
+                if (i == posY && j == posX) { 
+                    cell.setBackground(Color.GREEN);
+                    gameArray[i][j] = 1; // 1 = filled space
+                } else if (j % 2 == 1) {
+                    cell.setBackground(Color.LIGHT_GRAY);
+                }
             }
         }
+
 
         frame.add(panel);
         frame.pack();
--- SnakeGame.java
+++ SnakeGame.java
@@ -6,8 +6,9 @@ import java.util.ArrayList;
 public class SnakeGame {
     public static void main(String[] args) {
         Board board = new Board();
+        int startingX = 5, startingY = 5;
         int x = 20;
         int[][] grid = new int[x][x];  
-        board.initBoard(x);     
+        board.initBoard(x, startingX, startingY, grid);     
     }
 }
