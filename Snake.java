3fa8b92be61897a60ec6b9d58594d1d0007d33fb
Jsnit <jrsnitbro@gmail.com>
2023-11-04 13:25:37 -0700

    Continued updating snake

--- Snake.java
+++ Snake.java
@@ -11,7 +11,7 @@ public class Snake {
     int length; // length of the snake
 
 /*
-                                                             METHODS:
+                                                       METHODS:
 */
 
     public Snake() { // DVC initializing snake to 1 unit long, at (0,0) the top left corner of the grid, facing down
@@ -21,6 +21,7 @@ public class Snake {
         direction = 'd';
         length = occupiedSpaces.size();
     }
+
     public Snake(Pair<Integer, Integer> headInput) { // EVC initializing snake to 1 unit at headInput, facing down
         occupiedSpaces = new ArrayList<Pair<Integer, Integer>>();
         headIndex = new Pair<Integer, Integer>(headInput.first, headInput.second);
@@ -28,6 +29,7 @@ public class Snake {
         direction = 'd';
         length = occupiedSpaces.size();
     }
+
     public Snake(Pair<Integer, Integer> headInput, char directionInput) { // EVC initializing snake to 1 unit at headInput, facing directionInput
         occupiedSpaces = new ArrayList<Pair<Integer, Integer>>();
         headIndex = new Pair<Integer, Integer>(headInput.first, headInput.second);
@@ -35,6 +37,7 @@ public class Snake {
         direction = directionInput;
         length = occupiedSpaces.size();
     }
+
     public Snake(Pair<Integer, Integer> headIndexInput, ArrayList<Pair<Integer, Integer>> occupiedSpacesInput, char directionInput) { // EVC initializing snake to occupiedSpacesInput size at headInput, facing directionInput
         occupiedSpaces = new ArrayList<Pair<Integer, Integer>>();
         headIndex = new Pair<Integer, Integer>(headIndexInput.first, headIndexInput.second);
@@ -44,13 +47,30 @@ public class Snake {
         direction = directionInput;
         length = occupiedSpaces.size();
     }
-    private void addToFront(Pair <Integer, Integer> gridIndex) { // Adds a new value to the front of the snake
+
+    // Adds a new value to the front of the snake & sets head
+    private void addToFront(Pair <Integer, Integer> gridIndex) {
         occupiedSpaces.add(gridIndex);
         headIndex = gridIndex;
         length = occupiedSpaces.size();
     }
-    private void deleteFromEnd() { // Deletes the last value of the snake
+
+    // Deletes the last value of the snake
+    private void deleteFromEnd() {
         occupiedSpaces.remove(occupiedSpaces.size() - 1);
         length = occupiedSpaces.size();
+    }
+
+    // adds a new value to the front of the snake & sets head, deletes the last value of the snake
+    public void moveTo(Pair <Integer, Integer> gridIndex) {
+        addToFront(gridIndex);
+        deleteFromEnd();
+    }
+
+    // adds a new value to the front of the snake & sets head, deletes the last value of the snake if apple is false
+    public void moveTo(Pair <Integer, Integer> gridIndex, boolean apple) {
+        addToFront(gridIndex);
+        if (!apple)
+            deleteFromEnd();
     }
 }
